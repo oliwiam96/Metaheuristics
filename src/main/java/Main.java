9be21d7e5a4282905db7a9a109315017ff9819dd
OliwiaMasian <oliwia.m.96@wp.pl>
2019-10-15 16:01:44 +0200

    Add generating permutations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class Main {
     private static int MAX_COUNTER = 10;
     private static int MAX_MILLIS = 1000;
 
+    private static final void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
+    private static List<int[]> getPermutations(int N, int numberOfPermutations) {
+        List<int[]> permutations = new ArrayList<>();
+
+        for (int i = 0; i < numberOfPermutations; i++) {
+            int[] indexes = IntStream.range(0, N).toArray();
+            int lastIndex = N;
+            Random random = new Random();
+            for (int j = 0; j < N; j++) {
+                int randomInteger = random.nextInt(lastIndex);
+                swap(indexes, randomInteger, lastIndex - 1);
+                lastIndex -= 1;
+            }
+            permutations.add(indexes);
+        }
+
+        return permutations;
+    }
+
     private static void doAlgorithm() {
         int N = 10;
-        // TODO
+        List<int[]> permutations = getPermutations(N, 3);
     }
 
     private static double getCJU() {
@@ -16,7 +45,7 @@ public class Main {
             doAlgorithm();
             counter += 1;
             endTime = System.currentTimeMillis();
-        } while (endTime - startTime < MAX_MILLIS || counter < MAX_COUNTER);
+        } while (endTime - startTime < MAX_MILLIS && counter < MAX_COUNTER);
 
         return ((double) (endTime - startTime)) / counter;
     }
